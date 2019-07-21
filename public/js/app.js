6880ac0c82ff6b4eb10af382de7d41cbb80fbcd7
Mimi Niou <yniou@andrew.cmu.edu>
2019-07-21 01:18:13 -0400

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file is loaded.');
-
 // asynchronous io operation
 fetch('http://localhost:3000/weather?address=boston').then((response) => {
     response.json().then((data) => {
